6fba669c63ca7778b152037df54f8063827d6113
unknown <aishamirza0002@gmail.com>
2022-08-11 13:01:49 +0530

    added sticky navigation and completed the project

--- script.js
+++ script.js
@@ -11,3 +11,69 @@ openNaveEl.addEventListener("click", () => {
 closeNaveEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
+
+// smooth scrolling
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // scroll to home
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // scroll to links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // closing nav
+    if (link.classList.contains("nav-links")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+// sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    console.log(entry);
+    if (entry.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (entry.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+obs.observe(sectionHeroEl);
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
